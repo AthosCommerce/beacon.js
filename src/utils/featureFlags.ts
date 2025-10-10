e846b3a5ea482b7aeb97b97370e5ecebb7956904
Chris <chris.frazier@searchspring.com>
2025-10-10 13:12:20 -0700

    refactor(featureflags): remove unused code

--- src/utils/featureFlags.ts
+++ src/utils/featureFlags.ts
@@ -1,24 +1,7 @@
 export function getFlags(userAgent = ''): FeatureFlags {
 	userAgent = (userAgent || (typeof window == 'undefined' ? {} : window?.navigator).userAgent || '').toLowerCase();
 
-	const isIE = (function () {
-		let ieVersion: number | boolean;
-
-		return function isIE() {
-			if (ieVersion === undefined) {
-				const version = (userAgent.match(/(msie|trident\/7.0; rv:) ?([0-9]{1,2})\./) || [])[2];
-
-				ieVersion = version ? Number(version) : false;
-			}
-
-			return ieVersion;
-		};
-	})();
-
 	return {
-		cors: function () {
-			return !isIE() || Number(isIE()) >= 10;
-		},
 		cookies: function () {
 			return typeof window == 'undefined' ? false : window?.navigator?.cookieEnabled;
 		},
@@ -41,14 +24,12 @@ export function getFlags(userAgent = ''): FeatureFlags {
 }
 
 interface FeatureFlags {
-	cors: () => boolean;
 	cookies: () => boolean;
 	storage: () => boolean;
 }
 
 const flags = getFlags();
 export const featureFlags = {
-	cors: flags.cors(),
 	cookies: flags.cookies(),
 	storage: flags.storage(),
 };
